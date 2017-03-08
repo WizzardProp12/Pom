02e746466ecf06ea9f87ceb76da778eb4c171293
pompommer <woutercs@hotmail.com>
2017-03-09 00:34:55 +0100

    Add files via upload

--- OGP1617-Asteroids/src/asteroids/facade/Facade.java
+++ OGP1617-Asteroids/src/asteroids/facade/Facade.java
@@ -8,27 +8,36 @@ public class Facade implements IFacade {
 
 	@Override
 	public Ship createShip() throws ModelException {
-		
+		return createShip(0,0,0,0,1,Math.PI/2);
 		
 	}
 
 	@Override
 	public Ship createShip(double x, double y, double xVelocity, double yVelocity, double radius, double orientation)
 			throws ModelException {
-		 = x;
-		yCoord
+		Ship ship = new Ship();
+		ship.setXCoord(x);
+		ship.setYCoord(y);
+		ship.setXVelocity(xVelocity);
+		ship.setYVelocity(yVelocity);
+		ship.setRadius(radius);
+		ship.setOrientation(orientation);
+		return ship;
+		
+		 
 		
 	}
 
 	@Override
 	public double[] getShipPosition(Ship ship) throws ModelException {
-		// TODO Auto-generated method stub
-		return null;
+		return ship.getPositions();
+		
+		
 	}
 
 	@Override
 	public double[] getShipVelocity(Ship ship) throws ModelException {		
-		getShip
+		return ship.getVelocities();
 	}
 
 	@Override
@@ -38,50 +47,49 @@ public class Facade implements IFacade {
 
 	@Override
 	public double getShipOrientation(Ship ship) throws ModelException {
-		// TODO Auto-generated method stub
-		return 0;
+		return ship.getOrientation();
 	}
 
 	@Override
 	public void move(Ship ship, double dt) throws ModelException {
-		// TODO Auto-generated method stub
+		ship.move(dt);
 		
 	}
 
 	@Override
 	public void thrust(Ship ship, double amount) throws ModelException {
-		// TODO Auto-generated method stub
+		ship.thrust(amount);
 		
 	}
 
 	@Override
 	public void turn(Ship ship, double angle) throws ModelException {
-		// TODO Auto-generated method stub
+		ship.turn(angle);
 		
 	}
 
 	@Override
 	public double getDistanceBetween(Ship ship1, Ship ship2) throws ModelException {
 		// TODO Auto-generated method stub
-		return 0;
+		return ship1.getDistanceBetween(ship2);
 	}
 
 	@Override
 	public boolean overlap(Ship ship1, Ship ship2) throws ModelException {
 		// TODO Auto-generated method stub
-		return false;
+		return ship1.overlap(ship2);
 	}
 
 	@Override
 	public double getTimeToCollision(Ship ship1, Ship ship2) throws ModelException {
 		// TODO Auto-generated method stub
-		return 0;
+		return ship1.getTimeToCollision(ship2);
 	}
 
 	@Override
 	public double[] getCollisionPosition(Ship ship1, Ship ship2) throws ModelException {
 		// TODO Auto-generated method stub
-		return null;
+		return ship1.getCollisionPosition(ship2);
 	}
 
 }
