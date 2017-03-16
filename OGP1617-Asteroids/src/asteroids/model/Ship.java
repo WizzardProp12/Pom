06cd446d308c84d68f10c562650d5a44996422de
WizzardProp12 <woutercs@hotmail.com>
2017-03-16 12:53:09 +0100

    Add files via upload

--- OGP1617-Asteroids/src/asteroids/model/Ship.java
+++ OGP1617-Asteroids/src/asteroids/model/Ship.java
@@ -14,7 +14,15 @@ import be.kuleuven.cs.som.annotate.*;
  *         
  * 
  * @author Wouter Cams and Stijn Bruggeman
+ * 
+ * Studies:
+ * Wouter Cams: 2e Bachelor Ingenieurswetenschappen
+ * Hoofdrichting Elektrotechniek, nevenrichting computerwetenschappen
+ * Stijn Bruggeman: 2e Bachelor Ingenieurswetenschappen
+ * Hoofdrichting computerwetenschappen, nevenrichting elektrotechniek
+ * 
  * @version  0.1
+ * 
  * A link to the GitHub code depository:
  * https://github.com/WizzardProp12/Pom
  *
