1d45b4e74c772cf70373b5e073c0e7875b142ecd
WizzardProp12 <woutercs@hotmail.com>
2017-04-21 19:53:43 +0200

    Update CollisionType.java

--- OGP1617-Asteroids/src/asteroids/model/entities/CollisionType.java
+++ OGP1617-Asteroids/src/asteroids/model/entities/CollisionType.java
@@ -1,5 +1,21 @@
-package asteroids.model.entities;
+package asteroids.model;
 
+/**
+ * @author Wouter Cams and Stijn Bruggeman
+ * 
+ * Studies:
+ * Wouter Cams: 2e Bachelor Ingenieurswetenschappen
+ * Hoofdrichting Elektrotechniek, nevenrichting computerwetenschappen
+ * Stijn Bruggeman: 2e Bachelor Ingenieurswetenschappen
+ * Hoofdrichting computerwetenschappen, nevenrichting elektrotechniek
+ * 
+ * @version  0.1
+ * 
+ * A link to the GitHub code repository:
+ * https://github.com/WizzardProp12/Pom
+ * 
+ * An enum describing a type of collision.
+ */
 public enum CollisionType {
 	entity, topWall, bottomWall, leftWall, rightWall
 }
