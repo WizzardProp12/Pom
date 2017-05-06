04d9892fbe69c4468d31f41c2bfa20db38c35824
Wouter-Cams <woutercs@hotmail.com>
2017-05-06 12:42:47 +0200

    implemented asteroid and minorplanet classes

--- src/asteroids/model/Asteroid.java
+++ src/asteroids/model/Asteroid.java
@@ -1,9 +1,46 @@
 package asteroids.model;
 
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Immutable;
+import be.kuleuven.cs.som.annotate.Raw;
+
 public class Asteroid extends MinorPlanet{
+	
+	// CONSTRUCTORS
 
 	public Asteroid(double xCoord, double yCoord, double xVelocity, double yVelocity, double radius, World world) {
 		super(xCoord, yCoord, xVelocity, yVelocity, radius, world);
 		
 	}
+	
+	public Asteroid(double xCoord, double yCoord, double xVelocity, double yVelocity, double radius) {
+		super(xCoord, yCoord, xVelocity, yVelocity, radius);
+		
+	}
+	
+	// MASS (total)
+	
+	/**
+	 * The density of an Asteroid.
+	 */
+	private final double density = 2.65*Math.pow(10, 12);
+	
+	/**
+	 * Return the density of the Minor Planet.
+	 * @return see implementation...
+	 */
+	@Basic @Immutable @Raw
+	public double getDensity() {
+		return this.density;
+	}
+	/**
+	 * Return the mass of the Asteroid.
+	 * @return see implementation...
+	 */
+	@Override @Basic @Raw
+	public double getMass() {
+		return (4/3)*Math.PI*Math.pow(getRadius(), 3)*getDensity();
+	}
+	
+	
 }
