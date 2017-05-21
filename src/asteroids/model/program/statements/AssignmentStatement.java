db4ef66597edf3c53399e37e721507a879f28613
Wouter-Cams <woutercs@hotmail.com>
2017-05-21 18:00:16 +0200

    ene testpush

--- src/asteroids/model/program/statements/AssignmentStatement.java
+++ src/asteroids/model/program/statements/AssignmentStatement.java
@@ -1,22 +1,33 @@
 package asteroids.model.program.statements;
 
 
+import asteroids.model.program.Program;
+import asteroids.model.program.expressions.Expression;
 import asteroids.part3.programs.SourceLocation;
 
 
-public class AssignmentStatement extends Statement{
+public class AssignmentStatement<T> extends Statement{
 	private String variableName;
-	private Object value;
+	private Expression<T> value;
 	
-	public AssignmentStatement(String variableName, Object value,
+	public AssignmentStatement(String variableName, Expression<T> value,
 											SourceLocation sourceLocation){
 		super(sourceLocation);
 		this.variableName = variableName;
 		this.value = value;
+		
 	}
 
 	@Override
 	public void execute() {
 		getProgram().setVariable(variableName, value);
+	}
+	
+	@Override
+	public void setProgram(Program program){
+		super.setProgram(program);
+		value.setProgram(program);
+		System.out.println("assigned program to var");
+		
 	}
 }
